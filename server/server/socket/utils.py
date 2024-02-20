bbd4cf9095c8eec4ee1c0ec108eb8a46372099c2
dvip1 <dvipatwork@outlook.com>
2024-02-20 16:24:24 +0530

    refactor: game component, GameConsumer Socket

---
+++ server/server/socket/utils.py
@@ -0,0 +1,10 @@
+import random
+import string
+from .models import Game
+def generate_game_code():
+    length = 6 
+    from .models import Game
+    while True:
+            code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+            if not Game.objects.filter(code=code).exists():
+                return code 
--- server/server/socket/views.py
+++ server/server/socket/views.py
@@ -1,38 +1,35 @@
 from channels.generic.websocket import WebsocketConsumer
 import json
-import random
-import string
 
 
 class GameConsumer(WebsocketConsumer):
     def connect(self):
         try:
-            # from rest_framework_simplejwt.authentication import JWTAuthentication
-            # auth_header = self.scope['headers'].get(b'authorization') 
-            # token = auth_header.decode('utf-8').split()[1]
-            # authenticator = JWTAuthentication()
-            # user = authenticator.authenticate(token)[0]  # [0] to get the user
-            # self.scope['user'] = user
-
             self.accept()
+            self.send(text_data= json.dumps({
+                "type": "Success",
+                "message": "Connected ✅"
+            }))
         except Exception as e:
             print(f"Authentication error: {e}")
+            self.send(text_data=json.dumps({
+                "type": "error",
+                "message": "Not Connected ❌"
+            }))
             self.close()
 
     def receive(self, text_data):
-        data= json.loads(text_data)
-        if data['type']== 'create_game':
-            self.create_game()
+        try: 
+            data= json.loads(text_data)
+            if data['type']== 'create_game':
+                self.create_game()
+        except Exception as e: 
+            print(e)
+            self.send(e);
     
     def create_game(self):
         from .models import Game
-        def generate_game_code():
-            length = 6  # Customize the code length if you want
-            from .models import Game
-            while True:
-                code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
-                if not Game.objects.filter(code=code).exists():
-                    return code 
+        from .utils import generate_game_code
         code= generate_game_code()
         game= Game.objects.create(code=code, created_by= self.scope['user'])
         self.send(text_data= json.dumps({
@@ -45,14 +42,7 @@ class GameConsumer(WebsocketConsumer):
 class SimpleConsumer(WebsocketConsumer):
     def connect(self):
         try:
-            # from rest_framework_simplejwt.authentication import JWTAuthentication
-            # auth_header = self.scope['headers'].get(b'authorization') 
-            # token = auth_header.decode('utf-8').split()[1]
-
-            # authenticator = JWTAuthentication()
-            # user = authenticator.authenticate(token)[0]  # [0] to get the user
-            # self.scope['user'] = user
-
+            print(self.scope['user'])
             self.accept()
         except Exception as e:
             print(f"Authentication error: {e}")
